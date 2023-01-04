d3756255455be222a76768918b4e7f74bea2ebcd
Mohamed-98 <m.gado86@gmail.com>
2023-01-04 11:00:15 +0200

    password added

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,12 @@
+import { IsHash, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
   fname: string;
+  @IsNotEmpty()
   lname: string;
+  @IsNotEmpty()
   Role: string;
+  // @IsHash()
+  password: string;
 }
